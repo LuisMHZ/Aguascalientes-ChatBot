b201063ba47d1852c85f00cc86edcd988594735c
LuisMHZ <luismorquecho13@gmail.com>
2023-07-04 14:27:20 -0600

    Test de librería chatbot con OpenAI y ElevenLabs

---
+++ services/eventlab.js
@@ -0,0 +1,42 @@
+const fs = require('node:fs')
+
+/**
+ *
+ * @param {*} voiceId
+ * @returns
+*/
+
+const textToVoice = async (text,voiceId = 'pNInz6obpgDQGcFmaJgB') => {
+  const EVENT_TOKEN = process.env.EVENT_TOKEN ?? "";
+  const URL = `https://api.elevenlabs.io/v1/text-to-speech/${voiceId}`;
+
+  const header = new Headers();
+  header.append("accept", "audio/mpeg");
+  header.append("xi-api-key", EVENT_TOKEN);
+  header.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    text,
+    model_id: "eleven_multilingual_v1",
+    voice_settings: {
+      stability: 1,
+      similarity_boost: 0.8,
+    },
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: header,
+    body: raw,
+    redirect: "follow",
+  };
+
+  const response = await fetch(URL, requestOptions);
+  const buffer = await response.arrayBuffer();
+  const pathFile = `${process.cwd()}/tmp/${Date.now()}-audio.mp3`;
+  fs.writeFileSync(pathFile, Buffer.from(buffer));
+  
+  return pathFile;
+};
+
+module.exports = { textToVoice };
